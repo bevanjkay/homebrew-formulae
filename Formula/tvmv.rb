ec4b64e779b3c2764ca441d96072b7f139db81fa
Bevan Kay <email@bevankay.me>
2024-09-05 22:56:25 +0000

    tvmv: update 0.6.0 bottle.

--- Formula/tvmv.rb
+++ Formula/tvmv.rb
@@ -7,10 +7,10 @@ class Tvmv < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bevanjkay/formulae"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "4f673115552161ae89c0d8c20ed4e865ec8da2bf404cfbdd52f7030bc5686cc3"
-    sha256 cellar: :any_skip_relocation, ventura:      "4043c32383a6cb71fde7435da28a71db07038ff1789dcea60322740712d6f8da"
-    sha256 cellar: :any_skip_relocation, monterey:     "3568ef6564986f31d22c751c76ec4e350862ca7a7fbb3ecc5343ae2f80746fa4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3b7d13ea335650f67ef53579a59d13d25578dfdca5a02822fb81f578b6336e21"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "2e8215cfe47795bf8e892558f9cba37769ccf4dfdbd452c007cc44aa200df3aa"
+    sha256 cellar: :any_skip_relocation, ventura:      "e0a9645bfb6f5e72ad88a3485eb6cc13469164a08423718824828f0ee7796cc4"
+    sha256 cellar: :any_skip_relocation, monterey:     "6559400e21c22b81dc5d7343c819ef14d8ab51c8bfd700d7996b7e1ff15d10f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "efc72d54593246934cc470e6aad0873c58a9cbe6767e6049662a4a385bd635b1"
   end
 
   depends_on "ghc@9.4" => :build
