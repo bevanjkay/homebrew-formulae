64642c44b70a5393bdc782489cc6a71b9bc88249
Bevan Kay <email@bevankay.me>
2024-09-08 23:34:36 +1000

    surge-cli 0.24.6 (new formula)

---
+++ Formula/surge-cli.rb
@@ -0,0 +1,19 @@
+class SurgeCli < Formula
+  desc "CLI client for the surge.sh hosted service"
+  homepage "https://surge.sh/"
+  url "https://registry.npmjs.org/surge/-/surge-0.24.6.tgz"
+  sha256 "becf2678a30ad17c8384c36382f5c62e27ce73a9ba184c8664cea4ee40053ee0"
+  license "ISC"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", "--no-deprecation", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/surge --version")
+    assert_match "Not Authenticated", shell_output("#{bin}/surge whoami", 1)
+  end
+end
