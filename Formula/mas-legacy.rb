c630952a5c97bc848710266852b944646713dd07
Bevan Kay <email@bevankay.me>
2024-10-07 12:33:59 +0000

    mas-legacy: update 1.8.6_1 bottle.

--- Formula/mas-legacy.rb
+++ Formula/mas-legacy.rb
@@ -10,9 +10,10 @@ class MasLegacy < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bevanjkay/formulae"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "467c75e387264ca71d2169b487d2319e2a9d953445f518e012f601e64753fbd5"
-    sha256 cellar: :any_skip_relocation, ventura:      "61103d216596928bdf17ae6f12bbd695e7fa8bc4676a1512938df4682a07297b"
-    sha256 cellar: :any_skip_relocation, monterey:     "8b761b9d99057c89bb2eba0448fd30f9c24ce879f0afec6ba1027a7519ee18f1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "220d9b5a85ee95a6393ef6ab922caef4596a751a717e2dae240975f28b640a4b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9710343abbd55c7d367b8e1d17450bf86ebac35b1322e19b5973e56b2d505d70"
+    sha256 cellar: :any_skip_relocation, ventura:       "9f15bcbab85d0883647f301f6fae0cd280551d2fbb504d66b92b7dc6f61e6f95"
+    sha256 cellar: :any_skip_relocation, monterey:      "72e13bb368c1cb41af844fe0244b836624278cf11687ea366033cd6f9eaa538c"
   end
 
   depends_on :macos
