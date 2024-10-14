24df8b980320054e9242636d4c6641705fc7dc39
Bevan Kay <email@bevankay.me>
2024-10-14 11:56:36 +1100

    sevenzip-legacy : add formua

---
+++ Formula/sevenzip-legacy.rb
@@ -0,0 +1,39 @@
+class SevenzipLegacy < Formula
+  desc "7-Zip is a file archiver with a high compression ratio"
+  homepage "https://7-zip.org"
+  url "https://7-zip.org/a/7z2408-src.tar.xz"
+  version "24.08"
+  sha256 "aa04aac906a04df59e7301f4c69e9f48808e6c8ecae4eb697703a47bfb0ac042"
+  license all_of: ["LGPL-2.1-or-later", "BSD-3-Clause"]
+  head "https://github.com/ip7z/7zip.git", branch: "main"
+
+  livecheck do
+    url "https://7-zip.org/download.html"
+    regex(/>\s*Download\s+7-Zip\s+v?(\d+(?:\.\d+)+)\s+\([^)]+?\)/im)
+  end
+
+  def install
+    cd "CPP/7zip/Bundles/Alone2" do
+      mac_suffix = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch
+      mk_suffix, directory = if OS.mac?
+        ["mac_#{mac_suffix}", "m_#{mac_suffix}"]
+      else
+        ["gcc", "g"]
+      end
+
+      system "make", "-f", "../../cmpl_#{mk_suffix}.mak", "DISABLE_RAR_COMPRESS=1"
+
+      # Cherry pick the binary manually. This should be changed to something
+      # like `make install' if the upstream adds an install target.
+      # See: https://sourceforge.net/p/sevenzip/discussion/45797/thread/1d5b04f2f1/
+      bin.install "b/#{directory}/7zz"
+    end
+  end
+
+  test do
+    (testpath/"foo.txt").write("hello world!\n")
+    system bin/"7zz", "a", "-t7z", "foo.7z", "foo.txt"
+    system bin/"7zz", "e", "foo.7z", "-oout"
+    assert_equal "hello world!\n", (testpath/"out/foo.txt").read
+  end
+end
