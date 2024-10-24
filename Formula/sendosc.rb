5f4add5eb81e0df66f50bc12b138b1e8da642d22
Bevan Kay <email@bevankay.me>
2024-10-24 12:01:08 +0000

    sendosc: add 1.0.1 bottle.

--- Formula/sendosc.rb
+++ Formula/sendosc.rb
@@ -5,6 +5,14 @@ class Sendosc < Formula
   sha256 "5fb846a19c7881366969d8b91ffe8ed26d5062e9294a391dd96a245b8965f158"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bevanjkay/formulae"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96e6ecbb909af7ddaeb321ded7d0a362ae873f5d15f1dd589c53e090a4d3b907"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7d8df66ef23e3ffe1fc3fd11a6c1a72fa7adf3b7186d614162b0d91917a3c3e3"
+    sha256 cellar: :any_skip_relocation, ventura:       "80fd3c1f8e9a1238e807b9719884f836d10f683418ea32b6ef95a489e07e45b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "363ffc8143bc8dd4295d27717935e2775b20c042b4b013a4a21594739e02fe16"
+  end
+
   depends_on "node"
 
   def install
