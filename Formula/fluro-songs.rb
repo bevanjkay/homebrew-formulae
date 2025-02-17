9fb756d35979015504d5d39c6cd87a2ed5d1154c
Bevan Kay <email@bevankay.me>
2025-02-17 01:07:27 +0000

    fluro-songs: add a33a463077af21465b8acaf47048409d480ed6b3 bottle.

--- Formula/fluro-songs.rb
+++ Formula/fluro-songs.rb
@@ -15,6 +15,14 @@ class FluroSongs < Formula
     end
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/bevanjkay/formulae"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e671d509be1910b1e81805f9cdd07e9c1b2f8700afbbbc7e1dc9b345aec188ab"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0ee1aa46e6ca3a4b1217a9917dac269f9daa8446f26b768e68fb4604c9f92a0f"
+    sha256 cellar: :any_skip_relocation, ventura:       "ed9b34e2532bfd22f2fb94dc7019f8d7cf098b07aa4496e1e24c21d25a12518a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b22e473d695b67f6b286cb58450292cbfdb5eb5c7048bdbf9275224cb0acfdc3"
+  end
+
   depends_on "deno" => :build
 
   def install
