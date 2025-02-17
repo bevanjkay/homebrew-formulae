8248d01333fa17a60d0c7bd3aca148d292633256
Bevan Kay <email@bevankay.me>
2025-02-17 01:28:46 +0000

    bible: add 1e43e8e40cdac4734747362a34fce46f2e151579 bottle.

--- Formula/bible.rb
+++ Formula/bible.rb
@@ -15,6 +15,14 @@ class Bible < Formula
     end
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/bevanjkay/formulae"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34336e6ab08052fa46b6d651073d4a219652283720e7da6740a261c05042690b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0942b8bf8d0796d9915968c332acf7d6682c62aa55f1591d11a48e1df6c66ab5"
+    sha256 cellar: :any_skip_relocation, ventura:       "220302b6aee69488da0315535751cb50e2842f48ee2356ce3c42cbc3783264d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4298eb05b501cb5601e49e9efafdbf0a7fdfc3a8e9745673de904d5f28ae8a9"
+  end
+
   depends_on "deno" => :build
 
   def install
