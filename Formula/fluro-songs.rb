8ab89cc0f4b73e5d2c16a6dc1c7ccdb8d03f5096
Bevan Kay <email@bevankay.me>
2025-03-03 08:07:42 +0000

    fluro-songs: update aee3201c71f7e6ce52608884d16802f63ffcc350 bottle.

--- Formula/fluro-songs.rb
+++ Formula/fluro-songs.rb
@@ -15,10 +15,10 @@ class FluroSongs < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bevanjkay/formulae"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e671d509be1910b1e81805f9cdd07e9c1b2f8700afbbbc7e1dc9b345aec188ab"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0ee1aa46e6ca3a4b1217a9917dac269f9daa8446f26b768e68fb4604c9f92a0f"
-    sha256 cellar: :any_skip_relocation, ventura:       "ed9b34e2532bfd22f2fb94dc7019f8d7cf098b07aa4496e1e24c21d25a12518a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b22e473d695b67f6b286cb58450292cbfdb5eb5c7048bdbf9275224cb0acfdc3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "16447436f46e38e6b4b9e6dcd1a66a77ef1185a2072bddfb3f01ef06732bcb80"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1951b7b10b3f94b376f93dd805e2b877333701f363546b03bf9de630743189f9"
+    sha256 cellar: :any_skip_relocation, ventura:       "61e1217ceb3502e7ae937bffba76a5e5415775f7f54db07ebc3a156f547b346d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d2f56d1934b44f4ff8cc791f87da612c523d928ae6ff5e2bf95d365b2bfe2de4"
   end
 
   depends_on "deno" => :build
