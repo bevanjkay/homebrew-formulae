61cfecc5422c1fa27b992c3ad39a8ebd414760e6
Bevan Kay <email@bevankay.me>
2025-03-05 23:55:54 +1100

    pake 3.1.1 (new formula)

---
+++ Formula/pake.rb
@@ -0,0 +1,24 @@
+class Pake < Formula
+  desc "Turn any webpage into a desktop app with Rust with ease"
+  homepage "https://github.com/tw93/Pake"
+  url "https://registry.npmjs.org/pake-cli/-/pake-cli-3.1.1.tgz"
+  sha256 "ebeea895c247188a0817696818d1c0641c8a7440b84b4c7934504ffd9c6c4b56"
+  license "MIT"
+
+  depends_on :macos
+  depends_on "node"
+  depends_on "rust"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Delete native binaries installed by npm, as we dont support `musl` for a `libc` implementation
+    node_modules = libexec/"lib/node_modules/pake-cli/node_modules"
+    (node_modules/"@tauri-apps/cli-linux-x64-musl/cli.linux-x64-musl.node").unlink if OS.linux?
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pake --version")
+  end
+end
