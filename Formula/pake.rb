39e86e643cf63c4538042bfd8af72a056f192093
Bevan Kay <email@bevankay.me>
2025-03-05 13:29:44 +0000

    pake: add 3.1.1 bottle.

--- Formula/pake.rb
+++ Formula/pake.rb
@@ -5,6 +5,13 @@ class Pake < Formula
   sha256 "ebeea895c247188a0817696818d1c0641c8a7440b84b4c7934504ffd9c6c4b56"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bevanjkay/formulae"
+    sha256 cellar: :any, arm64_sequoia: "c4b3e0264cf9c18b333aadc0076351b0fa88515738e325e0fe1ef6ca8f558518"
+    sha256 cellar: :any, arm64_sonoma:  "9581b208e2123544fe87d439bbe1bce03b189563f47de94485912ac260a3e5de"
+    sha256 cellar: :any, ventura:       "5d00f8d1c9ea2be37b36b3efc5219ceeb0602f52503e2f9cd877b8fd825e6d87"
+  end
+
   depends_on :macos
   depends_on "node"
   depends_on "rust"
