5a6a0cf52911821f8f25dc7dbd8eeb8a439a51d9
Bevan Kay <email@bevankay.me>
2025-03-06 06:05:35 +0000

    bible: update 1.0.6 bottle.

--- Formula/bible.rb
+++ Formula/bible.rb
@@ -12,11 +12,10 @@ class Bible < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bevanjkay/formulae"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "314c57bebf33f2717b07e2d14c89a01f6c02c8c262a6f6440764cb05a6aa66e5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "512cf20635bb793ce9fd22a4f1bda0b5c7fbb5fb568a3d4119a57e6b588e705e"
-    sha256 cellar: :any_skip_relocation, ventura:       "008c44f01e567d249110c92046bc478b128fea2be57bb720b7cccbb68cb76fc7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d383a9a70b6a9058503522e0780cd0fa7c89375640ddc727c5be5d4a801811c6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d960582d387de50ca1f857f4a0afe134950692fa06c9eb3e93f7d2561761ceb1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2d6cb18f360d908d06b80cd33fb334d44ea76935483e5169183e95acc17198fa"
+    sha256 cellar: :any_skip_relocation, ventura:       "8dc2bd717c40de93ace91aff09cf817a2a677b0275254205b2bc71ae9f823f61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f5fcf820eaf900394e796d480643f07ed7f509094d387b8bce09be253a7360c4"
   end
 
   depends_on "deno" => :build
