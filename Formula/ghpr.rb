518f263304451c6e971c83aa655562d6ffba73b0
bevanjkay <email@bevankay.me>
2025-05-20 15:53:07 +1000

    ghpr 1.1.1

--- Formula/ghpr.rb
+++ Formula/ghpr.rb
@@ -1,8 +1,8 @@
 class Ghpr < Formula
   desc "Approve and automerge GitHub PRs"
   homepage "https://github.com/bevanjkay/custom-scripts"
-  url "https://github.com/bevanjkay/custom-scripts/archive/refs/tags/ghpr-1.1.0.tar.gz"
-  sha256 "75d6513c79ac478adbb5197532af0e356b97fafc8d0056ddcb8b97c7c148a87d"
+  url "https://github.com/bevanjkay/custom-scripts/archive/refs/tags/ghpr-1.1.1.tar.gz"
+  sha256 "d36b27c8c4f26c4079013e3c12179b6b63a9ac103a64ee82ad0e5f39399c03a1"
   license "MIT"
 
   livecheck do
