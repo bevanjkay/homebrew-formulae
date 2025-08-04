3d6b1e9959517f1451cff8689c64cb4ecd4287c3
Bevan Kay <email@bevankay.me>
2025-08-04 15:51:20 +1000

    Update ladybird.rb

--- Formula/ladybird.rb
+++ Formula/ladybird.rb
@@ -2,7 +2,7 @@ class Ladybird < Formula
   desc "Truly independent web browser"
   homepage "https://ladybird.org"
   url "https://github.com/LadybirdBrowser/ladybird/archive/4e6da3b14acfe39e995cf616349fc7e9efe3fe29.tar.gz"
-  sha256 "a5ed183847955644546a60b0baa0c1065058cbd8f7a95f31301742e49d029b62"
+  sha256 "4e67c92b345f5f482a839bb4983e1dd7a659e0254a49033171134ba445215d6f"
   license "BSD-2-Clause"
 
   depends_on "autoconf" => :build
