e3b2d7229fe426ba54867c8c422c20d97c8113b5
Bevan Kay <email@bevankay.me>
2025-09-04 02:22:52 +0000

    sendosc: update 1.0.1_1 bottle.

--- Formula/sendosc.rb
+++ Formula/sendosc.rb
@@ -8,10 +8,11 @@ class Sendosc < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bevanjkay/formulae"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96e6ecbb909af7ddaeb321ded7d0a362ae873f5d15f1dd589c53e090a4d3b907"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7d8df66ef23e3ffe1fc3fd11a6c1a72fa7adf3b7186d614162b0d91917a3c3e3"
-    sha256 cellar: :any_skip_relocation, ventura:       "80fd3c1f8e9a1238e807b9719884f836d10f683418ea32b6ef95a489e07e45b9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "363ffc8143bc8dd4295d27717935e2775b20c042b4b013a4a21594739e02fe16"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4bbc7b7e9022ecb566134555dbb0de76dcedb5505d93fc06b8c4f7a9fe8c086c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cf05a42294a39248c4c5f9caaf4594d2547ef9c598035ae5ef24cf66f2a0ee8c"
+    sha256 cellar: :any_skip_relocation, ventura:       "cfcdaff7230a6c43586b25e7a4e9e2ea3f65821e67b049dc61c07c65e578065e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "249424b11b458108c559658fe2fc10318aebefa3fe041618e108a24fec65067a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "42d16822b546c4f2147c6db0457349b53223b241adf08b506ef385e688e0339f"
   end
 
   on_ventura :or_newer do
