87d6e3da7b4be46a85aaf694f18369217857b093
Bevan Kay <email@bevankay.me>
2025-12-18 04:10:13 +0000

    pake: update 3.6.4 bottle.

--- Formula/pake.rb
+++ Formula/pake.rb
@@ -7,9 +7,9 @@ class Pake < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bevanjkay/formulae"
-    sha256 cellar: :any, arm64_tahoe:   "686ffadb1c33d4c2e0d91a60d780be9f18657c3f5635449c0bff22cc69279925"
-    sha256 cellar: :any, arm64_sequoia: "4d69003bc3f9d3f5a32e004ee436c97413f68b4119e1b229f820c86d64e3b48e"
-    sha256 cellar: :any, arm64_sonoma:  "795c60e623380d2f0c82e61cc1a09649db67e691e3db83fcfe63eb8a313b7971"
+    sha256 cellar: :any, arm64_tahoe:   "63fcd0f90d1acf4f2ed032a8d5ba8c96d838e14e995ada85fa82f0f195061fe4"
+    sha256 cellar: :any, arm64_sequoia: "8937b244b9701387182ac0598fc60a90d3e7e1cf9bfe8ccd910549cafb3d2e48"
+    sha256 cellar: :any, arm64_sonoma:  "907fd8ce65622a6fca6e7ec4bf008de23467b5fffdfae71604fda241d2048ebe"
   end
 
   depends_on :macos
