fb438b4ce8c7c1a58f9d73a9db033fc7b8ad0003
Bevan Kay <email@bevankay.me>
2026-03-03 22:45:39 +0000

    qik-mcp-server: update 6.12.0 bottle.

--- Formula/qik-mcp-server.rb
+++ Formula/qik-mcp-server.rb
@@ -7,11 +7,11 @@ class QikMcpServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bevanjkay/formulae"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ab9ba7bab595c7ca92343a27bbe66659ce59fbdbdc03a10d0e6d972407f5463a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb3dbd31f2ee1e1a6786029121164ac40f86e75601f2556b0f65b079c0e6c1ff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "992c7583ef2248c5727726c57ef1e72e71001e03eb1b8ac16071589350ec8cdd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "aaa4385ddd5ff8147db60e5f7a3fb52a39bdd994aab16a7777f83a24dfe17605"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3298901c8c6cfbeb895e7a60e41917c2206f4118a997752a3433b472a8a5d9ad"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "01f9b1adce8f6966b58fcef3b3e933a0b6acba4a237e7510d8c9eecba5bf692b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff377805df0772774d703390ba91013ea61a70df2d19bddad9b59edd6c7a52fd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6cc9da200e81730d6e3b2d4e54e49ac972da9f2edcbc6fbf141fe082b8bf36e7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "01c28ccedc7bc6b01f8e2d1976fdd45b21f02814df111ef47906e78d74137acf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc42de00a40bba45aac0a8fe4bfb99f7fa14def239b88428fcd2cc4bdc0a117f"
   end
 
   depends_on "node"
