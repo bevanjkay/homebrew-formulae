454bb2613e14db14f206c42f9669681d5a65bb10
Bevan Kay <email@bevankay.me>
2026-07-30 04:46:49 +0000

    wacli: add 0.15.0 bottle.

--- Formula/wacli.rb
+++ Formula/wacli.rb
@@ -6,6 +6,15 @@ class Wacli < Formula
   license "MIT"
   head "https://github.com/openclaw/wacli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bevanjkay/formulae"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6b39b6caccecaf1b57ca66fc87c999d50ca6dc9187cb9db23b3062473895e19b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da73a410009a87088b1c5f147a1e8718b881e179ab1173062f6f97ee74ee46ec"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "63c4df69fe309d7c7e98f13b2b90fe3e964243c99700f33c7ec7d280b8383c7d"
+    sha256 cellar: :any,                 arm64_linux:   "e308e2deb45953e9a8c4d564edc66f211da868a4fb2c3d4d69f2fe365f8b9399"
+    sha256 cellar: :any,                 x86_64_linux:  "1f6d66b567fdbb303c405216cf8cf557b611db8fc809958e271ff23acc78a98c"
+  end
+
   depends_on "go" => :build
 
   def install
