22cad79d0a5637038f8a094574824f7fc526449a
Bevan Kay <email@bevankay.me>
2026-08-25 05:55:29 +0000

    untrunc: add 2026.08.19 bottle.

--- Formula/untrunc.rb
+++ Formula/untrunc.rb
@@ -8,6 +8,15 @@ class Untrunc < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/anthwlock/untrunc.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bevanjkay/formulae"
+    sha256 cellar: :any, arm64_tahoe:   "d873e6383a2e72ca5446165bb1d8fe10982d2263c08de15a5230a5cd7854b56d"
+    sha256 cellar: :any, arm64_sequoia: "df02c890c87a715cde1aa59fce1fc5347781a1fd801bcefb300b21d8a7c86125"
+    sha256 cellar: :any, arm64_sonoma:  "4ca001194a0f86314a3fdae9aec44e8578dc0ea8ae50ab45e1efb4a78500cf54"
+    sha256 cellar: :any, arm64_linux:   "be32e4d63d5c1c560247fc3e177210c2b433a011c301ddfc61090f895a856e2a"
+    sha256 cellar: :any, x86_64_linux:  "2d645de1ac7b8b637839259f221c79955c0542ff9aad24ffa1edb0ee46956960"
+  end
+
   depends_on "ffmpeg"
 
   def install
