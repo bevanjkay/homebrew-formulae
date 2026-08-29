4a083585c0d32a2e7b37ca8950aa4a5ae516d006
Bevan Kay <email@bevankay.me>
2026-08-29 22:22:20 +0000

    surge-cli: update 0.44.0 bottle.

--- Formula/surge-cli.rb
+++ Formula/surge-cli.rb
@@ -7,11 +7,11 @@ class SurgeCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bevanjkay/formulae"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c08b094d65d09f6664f08d5a36e5cf4a56f09c78d8d9c9f944f326df48e69a4d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7a72d86c90076e3e48ea7e4a5354083872eae4a1ca65241f44c256ff05822f69"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2b63d279b4ad8cb6d0b9e83154ccd50b2345d03078fb5e8dfd598f36f8bd73b6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e61933baa9596037a68fc90b96fcb28733a7f46b407245388cc48a94e83ece52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d439589002fa440ace0727fea107a4a57457d0a4b936aba73f37dc0c01104560"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f180745870d7052602a46fd458223a927c5909a1c697e8ed9d1e9ff08ed29db1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e626dc807205f6ce3d35f29cacf6d7373f2d0591243df1b7c1fcca4e189ee028"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c49e5e13ec1348b267124ebc3b9cb1ae101431217bce01dd42178ecbe870c052"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d448883a1c8b75da207a726bafdb52f19d8bbc2a6a573af7731fec1821f59930"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "901b78e0d667cb9f51446667633975399ca69b9be5b5958fc08917d18d8320f4"
   end
 
   depends_on "node"
